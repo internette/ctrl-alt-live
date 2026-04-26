3ba4f13caa5f73bc6fef9c439aecfaebd6dba16e
Antoinette Janus <antoinettejanus@Antoinettes-Laptop.local>
2026-04-25 19:02:05 -0700

    Add monster creation setup

--- adopt-a-feeling/src/App.tsx
+++ adopt-a-feeling/src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import FeelingForm from './components/form/FeelingForm.tsx'
 import MonsterArea from './components/monsterArea/MonsterArea';
 import './App.css'
 
 function App() {
+  const [monster, setMonster] = useState<string>('');
+
   return (
     <div className="App">
       <h1>Adopt a Feeling</h1>
       <main>
-        <FeelingForm />
-        <MonsterArea />
+        <FeelingForm setMonster={setMonster} />
+        <MonsterArea monster={monster} />
       </main>
     </div>
   )
--- adopt-a-feeling/src/components/monsterArea/MonsterArea.tsx
+++ adopt-a-feeling/src/components/monsterArea/MonsterArea.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const MonsterArea: React.FC = () => {
+interface MonsterAreaProps {
+  monster: string;
+}
+
+const MonsterArea: React.FC<MonsterAreaProps> = ({ monster }) => {
   return (
     <div id="monster-pen">
-      {/* Monster content will go here */}
+      {monster ? (
+        <img src={monster} alt="Generated Monster" style={{ maxWidth: '100%', height: 'auto' }} />
+      ) : (
+        <p>Your monster will appear here after you submit the form!</p>
+      )}
     </div>
   );
 };
